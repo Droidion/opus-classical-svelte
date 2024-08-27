ca31eecd104f70d1560939c0ab778f0ab86eee0f
Denis Rodionov <nicholas.hawkwood@gmail.com>
2024-08-27 10:42:54 +0300

    fix styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-  import '../app.css';
-  import Footer from '$components/layout/Footer.svelte';
-  import Header from '$components/layout/Header.svelte';
+  import "../app.css";
+  import Footer from "$components/layout/Footer.svelte";
+  import Header from "$components/layout/Header.svelte";
 </script>
 
-<div class="grid justify-items-center min-h-screen w-full grid-rows-[auto_1fr_auto]">
-  <main class="main flex flex-col w-full max-w-screen-xl overflow-auto px-4 pb-4">
-    <Header />
+<div
+  class="grid justify-items-center min-h-screen w-full grid-rows-[auto_1fr_auto]"
+>
+  <Header />
+  <main
+    class="main flex flex-col w-full max-w-screen-xl overflow-auto px-4 pb-4"
+  >
     <slot></slot>
-    <Footer />
   </main>
+  <Footer />
 </div>
