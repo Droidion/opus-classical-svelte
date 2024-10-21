defe03de2e73c32178a6589fd7b0d0602bb37c5f
Denis Rodionov <nicholas.hawkwood@gmail.com>
2024-10-21 12:14:22 +0300

    disable prepare

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,5 +2,5 @@ import { env } from '$env/dynamic/private'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
-const client = postgres(env.DATABASE_URL)
+const client = postgres(env.DATABASE_URL, { prepare: false })
 export const db = drizzle(client)
